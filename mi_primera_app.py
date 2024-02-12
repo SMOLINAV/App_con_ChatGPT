8a93916b8014db4ca9de5ddfef2e79eae50c2c05
SMOLINAV <111089184+SMOLINAV@users.noreply.github.com>
2024-02-12 16:27:55 -0500

    Update mi_primera_app.py

--- mi_primera_app.py
+++ mi_primera_app.py
@@ -1,1 +1,17 @@
+# app_streamlit.py
+
+import streamlit as st
+
+# Título
+st.title("Mi primera app")
+
+# Autor
+st.write("Esta app fue elaborada por COLOQUE AQUÍ SU NOMBRE")
+
+# Pregunta al usuario su nombre
+nombre_usuario = st.text_input("Por favor, ingresa tu nombre")
+
+# Imprimir mensaje de bienvenida
+if nombre_usuario:
+    st.write(f"{nombre_usuario}, te doy la bienvenida a mi primera app.")
 
