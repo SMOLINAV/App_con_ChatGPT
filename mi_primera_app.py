d5da1cc4e93875aaeaad36b052d443788e740223
SMOLINAV <111089184+SMOLINAV@users.noreply.github.com>
2024-02-12 16:31:45 -0500

    Update mi_primera_app.py

--- mi_primera_app.py
+++ mi_primera_app.py
@@ -6,7 +6,7 @@ import streamlit as st
 st.title("Mi primera app")
 
 # Autor
-st.write("Esta app fue elaborada por Santiago")
+st.write("Esta app fue elaborada por Santiago M")
 
 # Pregunta al usuario su nombre
 nombre_usuario = st.text_input("Por favor, ingresa tu nombre")
